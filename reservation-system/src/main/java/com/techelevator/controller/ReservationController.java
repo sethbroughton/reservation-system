a8eeb67260418aee691c7b15dd2c8bf3598b7028
Seth Broughton <broughton.24@gmail.com>
2020-03-29 19:38:36 -0400

    Added user authentication

--- reservation-system/src/main/java/com/techelevator/controller/ReservationController.java
+++ reservation-system/src/main/java/com/techelevator/controller/ReservationController.java
@@ -1,6 +1,15 @@
 package com.techelevator.controller;
 
 import java.time.LocalDateTime;
+
+import com.techelevator.authentication.AuthProvider;
+import com.techelevator.authentication.UnauthorizedException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -12,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.techelevator.authentication.UnauthorizedException;
 import com.techelevator.model.Rate;
 import com.techelevator.model.RateDao;
 import com.techelevator.model.Reservation;
@@ -40,10 +50,23 @@ public class ReservationController {
 	@Autowired
 	private ReservationDao reservationDao;
 	
+    @Autowired
+    private AuthProvider auth;
+	
 	public static final String APPLICATION_KEY = "reservation";
 
-	@RequestMapping(path = {"/","home"}, method = RequestMethod.GET)
-	public String displayHomePage(ModelMap modelHolder) {
+    @RequestMapping(path = "/private", method = RequestMethod.GET)
+    public String privatePage() throws UnauthorizedException {
+        if (auth.userHasRole(new String[] { "admin", "user" })) {
+            return "private";
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+	
+	
+	@RequestMapping(path = {"/"}, method = RequestMethod.GET)
+	public String privatePage(ModelMap modelHolder) {
 
 		// List all current reservations
 		// Find available dates
